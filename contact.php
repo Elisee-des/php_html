92788aeb50b87be729ea7dca9a5390112992edb5
Elisee-des <esabidani@gmail.com>
2022-06-18 14:54:56 +0000

    initialisation du projet

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php require "header.php"; ?>
+
+<h2>Nous contacter</h2>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti earum repudiandae nulla nihil necessitatibus corrupti maiores, odio dicta asperiores ut? Facere ipsum id laboriosam iusto, non similique molestiae. Possimus cumque mollitia minima quasi totam cum ad saepe omnis id, dolorem perspiciatis, quae ipsa qui sit, tenetur et vitae! Architecto nostrum suscipit, optio, facilis, provident doloremque hic iste rerum in accusamus quo fugit autem deserunt possimus nihil aspernatur laboriosam quisquam quos. Esse est mollitia aperiam. Aperiam molestiae, tempora necessitatibus doloribus explicabo fugiat. Animi sunt possimus quos quod! Asperiores error ut ab sint mollitia maxime cum numquam quae doloremque molestias voluptate, ipsa consectetur fugiat reiciendis vero sed distinctio minima perspiciatis deleniti quidem! Mollitia, hic accusantium? Sapiente quis, esse doloribus assumenda rem placeat animi, nemo culpa deserunt laborum, dicta distinctio ad? Eveniet accusamus, repudiandae numquam reprehenderit et blanditiis! Sunt nobis, doloremque ullam tempore non itaque ea eaque repellendus dolorem impedit, blanditiis iusto quam modi, quas repellat voluptates consectetur soluta! Quisquam autem iure pariatur exercitationem provident nemo praesentium. Ea corrupti omnis adipisci, vero qui fugiat, et tempore eos pariatur debitis est voluptas cum quos vitae nulla officiis laboriosam quia voluptate recusandae. Recusandae facere maxime, laboriosam, cumque vitae ad ex ab beatae repellat similique impedit?
+</p>
+
+<?php require "footer.php"; ?>
---
+++ header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+  <title><?php echo $titre ?></title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="starter-template.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <a class="navbar-brand" href="#">Mon site</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/index.php">Acceuil <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact.php">contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
